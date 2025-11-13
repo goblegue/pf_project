5da3ab1f90c8fcd648080e9e8fcd6faaed18f2e9
goblegue <101870464+goblegue@users.noreply.github.com>
2025-11-13 22:57:46 +0500

    candyGrid inialized

--- src/board.cpp
+++ src/board.cpp
@@ -1,3 +1,23 @@
 #include "utils/Random.hpp"
 #include "board.hpp"
 
+void initalizeGrid(Board &gameBoard)
+{
+    Candy newCandy{};
+
+    for (int i{0}; i < MAX_ROWS; i++)
+    {
+        for (int j{0}; j < MAX_COLUMNS; j++)
+        {
+            newCandy.isMarkedDeletion = false;
+            newCandy.special = None;
+            CandyColor candyColor;
+            do
+            {
+                candyColor = static_cast<CandyColor>(getRand(0, 4));
+            } while ((j >= 2 && gameBoard.candyGrid[i][j - 2].color == candyColor && gameBoard.candyGrid[i][j - 1].color == candyColor) || (i >= 2 && gameBoard.candyGrid[i - 2][j].color == candyColor && gameBoard.candyGrid[i - 1][j].color == candyColor));
+            newCandy.color = candyColor;
+            gameBoard.candyGrid[i][j] = newCandy;
+        }
+    }
+}
